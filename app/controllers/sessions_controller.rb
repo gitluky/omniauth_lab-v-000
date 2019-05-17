37470be6a088ee2bab55f419f2f3b7572425ed59
Jonathan Luk <jluk87@gmail.com>
2019-05-17 01:47:38 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   def create
     # binding.pry
     @auth = request.env['omniauth.auth']
-    user = User.find_or_create_by(uid: @auth.uid) do |u|
+    user = User.find_or_create_by(uid: @auth[:uid]) do |u|
         u.email = @auth[:info][:email]
         u.name = @auth[:info][:name]
     end
