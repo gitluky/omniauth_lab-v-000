a9de3db79ca200c15d1a72920dd30cc2ef117094
Jonathan Luk <jluk87@gmail.com>
2019-05-17 01:24:10 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,10 @@
 class SessionsController < ApplicationController
 
   def create
-
+    auth_hash = request.env['omniauth.auth'][:info]
+    user = User.find_or_create_by(email: auth_hash[:email])
+    session[:user_id] = user.id
+    redirect_to ''
   end
 
 end
