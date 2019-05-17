6ba11331197ec1aaa7cde0f1da6df1bf31a5541f
Jonathan Luk <jluk87@gmail.com>
2019-05-17 01:43:25 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
   def create
+    binding.pry
     @auth = request.env['omniauth.auth']
     user = User.find_or_create_by(email: @auth[:info][:email], name: @auth[:info][:name])
     session[:user_id] = user.id
